cfb859a8a2351e99e334b4fcf62cfee0e2403768
Sanjay C <sanjaycs483@gmail.com>
2026-01-24 22:51:00 +0530

    Left Angled Triangle inverted

---
+++ src/basics/LeftTriangleInverted.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class LeftTriangleInverted {
+    public static void main(String [] args){
+        for(int i = 3; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
