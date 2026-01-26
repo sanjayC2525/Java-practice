da74013c32497d8f468b3346449bcf1c1b281e91
Sanjay C <sanjaycs483@gmail.com>
2026-01-26 14:14:18 +0530

    Diamond Pattern with 2 Approach

---
+++ src/AdvancePattern/DiamondPattern.java
@@ -0,0 +1,50 @@
+package AdvancePattern;
+public class DiamondPattern {
+    public static void main(String []args){
+        int n =5;
+        //first Method
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // Second Method
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<= 2*i-1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
